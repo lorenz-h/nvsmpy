f2ead51ea4c03b3cba79bb88f1807992212dde85
Lorenz Hetzel <32291082+lorenz-h@users.noreply.github.com>
2020-12-15 11:55:30 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup_kwargs = {
     "name": "nvsmpy",
-    "version": "0.2.1",
+    "version": "0.2.2",
     "author": "lorenz-h",
     "author_email": "lorenz.hetzel@yahoo.de",
     "description": "Find unoccupied GPUs on multi-user systems",
